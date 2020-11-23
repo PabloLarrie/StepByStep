b92eb88c6d5182133111758d0a5eddaf0ed02288
Larrie <pablo_14_pont@hotmail.com>
2020-11-23 18:17:34 +0100

    Basic del todo

--- stepbystep/urls.py
+++ stepbystep/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('fotos/', views.fotos, name='fotos'),
+    path('email/', views.email, name='email'),
+    path('contacto/', views.contacto, name='contacto'),
 ]
