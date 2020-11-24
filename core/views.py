d17a745abebfd9b9fba2d98203cb4231e0dbb99b
Larrie <pablo_14_pont@hotmail.com>
2020-11-24 15:03:45 +0100

    Miweb0.1

--- core/views.py
+++ core/views.py
@@ -2,26 +2,16 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-html_base = """
-<h1>Mi web personal</h1>
-<ul> 
-    <li><a href='/'> Home </a></li>
-    <li><a href='/fotos/'> Fotos </a></li>
-    <li><a href='/email/'> Email </a></li>
-    <li><a href='/contacto/'> Contacto </a></li>
-</ul>
-"""
-
-
 
 def home(request):
-    return HttpResponse (html_base + "<h2>Hola amigos del curso. Gracias por atender.</h2>")  
+    return render (request, "core/home.html")  
 
 def fotos(request):
-    return HttpResponse (html_base + "<h3>Aquí vemos nuestras fotos.</h3>")    
+    return render (request, "core/fotos.html")      
 
 def email(request):
-    return HttpResponse (html_base +"<h2>Aqui dejad vuestro email.</h2>")    
+    return render (request, "core/email.html")    
 
 def contacto(request):
-    return HttpResponse (html_base + "<h2>Si tenéis dudas, pues preguntar joder.</h2>")     
+    return render (request, "core/contacto.html")      
+
